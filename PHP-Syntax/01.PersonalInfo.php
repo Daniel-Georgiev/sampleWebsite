1d4883418b6116247e17e4975c233c3ef155a0fc
Daniel-Georgiev <daniko91@mail.bg>
2014-12-14 23:00:40 +0200

    update

--- PHP-Syntax/01.PersonalInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $firstName = 'Daniel';
-  $lastName = 'Georgiev';
-  $age = 19;
-  $name = $firstName.' '.$lastName;
-  echo 'My name is ' . $name .' and I am ' .$age .' years old.'
-
-?>
--- PHP-Syntax/02.sumTwoNumbers.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $firstInteger = 55.5454534;
-    $secondInteger = 155.550;
-    $sum =  $firstInteger + $secondInteger;
-    echo "\$firstInteger + \$secondInteger = $firstInteger + $secondInteger = ". number_format((float)$sum, 2, '.', '');;
-
-?>
